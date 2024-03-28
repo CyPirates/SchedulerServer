7ddf478b091ab9ed830a03a49aee5a64699d848f
DEMl4N <delfirmamentos@gmail.com>
2024-03-28 19:15:46 +0900

    Feat: CI/CD 설정

---
+++ src/main/java/com/example/schedulerserver/domain/test/entity/Test1.java
@@ -0,0 +1,23 @@
+package com.example.schedulerserver.domain.test.entity;
+
+import com.example.schedulerserver.global.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+//@Entity
+//@Table(name = "test1")
+//@Getter
+//@Setter
+//@SuperBuilder
+//@NoArgsConstructor
+//@AllArgsConstructor
+public class Test1  {
+//    @Column(name = "name", nullable = false)
+//    private String name;
+}
