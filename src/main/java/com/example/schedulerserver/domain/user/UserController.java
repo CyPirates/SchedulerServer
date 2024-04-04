bea55425f90ede23eec591fdbd22bec1f283f9ac
jhsay0314 <jhsay0314@gmail.com>
2024-04-04 23:37:28 +0900

    signup

--- src/main/java/com/example/schedulerserver/domain/user/UserController.java
+++ src/main/java/com/example/schedulerserver/domain/user/UserController.java
@@ -2,20 +2,22 @@ package com.example.schedulerserver.domain.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
+@RestController
+@Slf4j(topic = "signup controller")
 @RequiredArgsConstructor
-@Controller
 @RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
-
 
     @PostMapping("/signup")
     public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult){
