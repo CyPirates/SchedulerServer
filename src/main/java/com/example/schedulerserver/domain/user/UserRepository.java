adc18156e8d434d8f65b34f8c30788e46350a6e4
jhsay0314 <jhsay0314@gmail.com>
2024-04-09 12:30:21 +0900

    4.09

--- src/main/java/com/example/schedulerserver/domain/user/UserRepository.java
+++ src/main/java/com/example/schedulerserver/domain/user/UserRepository.java
@@ -2,5 +2,9 @@ package com.example.schedulerserver.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser,Long> {
+    Optional<SiteUser> findByUser_id(String user_id);
 }
